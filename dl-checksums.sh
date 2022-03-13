4576d2b62e7bd9ea83277272ad36072c48baa1f4
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-03-12 21:46:36 -0500

    cluster_api:1.1.3

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -16,7 +16,7 @@ dl()
     local url=$MIRROR/v${ver}/${APP}-${platform}
     if [ ! -e $lfile ]
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
@@ -32,4 +32,4 @@ dl_ver ()
     dl $ver linux arm64
 }
 
-dl_ver ${1:-1.1.2}
+dl_ver ${1:-1.1.3}
